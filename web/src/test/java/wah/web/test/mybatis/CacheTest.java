99fdebe31ae53854cfd6c467aea6ca74337f1768
Liuqc2008 <313624981@qq.com>
2019-01-05 11:37:14 +0800

    3232

---
+++ web/src/test/java/wah/web/test/mybatis/CacheTest.java
@@ -0,0 +1,21 @@
+package wah.web.test.mybatis;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/applicationContext.xml")
+public class CacheTest {
+	
+	
+	
+	@Ignore
+	@Test
+	public void GetSqlSession() {
+		
+		
+	}
+}
